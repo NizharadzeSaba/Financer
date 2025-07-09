83610e6f622925fad268ba451762a8650a9c0193
Niko Tsiklauri <n.tsiklauri02@gmail.com>
2025-07-09 19:55:49 +0400

    Update transactions.tsx

--- app/(app)/transactions.tsx
+++ app/(app)/transactions.tsx
@@ -52,7 +52,6 @@ export default function Transactions() {
         <RefreshControl refreshing={isFetching} onRefresh={refetch} />
       }
       onEndReached={() => {
-        console.log(hasNextPage);
         if (hasNextPage && !isFetchingNextPage) {
           fetchNextPage();
         }
